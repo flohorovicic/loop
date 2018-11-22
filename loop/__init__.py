e5fae54b89c66d95be63191b240480d76e597d64
Jan Florian Wellmann <jan.florian.wellmann@gmail.com>
2018-11-22 14:40:31 +1100

    Notebook and init file added

---
+++ loop/__init__.py
@@ -0,0 +1,17 @@
+"""Stochastic geological modeling and inversion"""
+
+import matplotlib.pyplot as plt
+
+
+
+class loop_project(self):
+    """Class definition for loop project"""
+
+    def load_data(self, filename, **kwds):
+        """Load csv data into project"""
+        pass
+
+    def interpolate(self, **kwds):
+        """Interpolate values"""
+        pass
+
